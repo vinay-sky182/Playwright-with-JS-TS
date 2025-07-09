170c72adb59e8a9ada17a1f6ab85f02c22c87c3d
vinay-sky182 <vinay.sky182@gmail.com>
2025-07-09 21:26:26 +0530

    Add JavaScript basics: conditional statements, switch case, template literals, and truthy/falsy values

---
+++ basics/templateliterals.js
@@ -0,0 +1,12 @@
+// template literals `` are a way to create multi-line strings and embed expressions in JavaScript.
+
+console.log(`-----template literals----`);
+
+let msg = `Hi my name is Vinay this is my JS code
+              and I\`m learning JavaScript.`;
+
+console.log(msg);  
+
+let name = "Vinay";
+let age = 25;
+console.log(`hey ${name}, you are ${age} years old.`); // Using template literals to embed variables
---
+++ basics/truthyfalsy.js
@@ -0,0 +1,7 @@
+// 8 flasy values in js:
+// 0n - BigInt zero
+
+// other than these 8 vlues are considered truthy in JavaScript.
+
+const falsyValues = [false, 0, -0, "", null, undefined, NaN, 0n];
+
