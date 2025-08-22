5e9cd793c1a0d9ded487dc742375d5b82fe53b22
Vinay Gupta <vigupta@keywordsstudioa.com>
2025-08-22 09:24:21 +0530

    Initial commit for TypeScript Tutorial

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+let username = 'tom';
+console.log(username);
+let age = 32;
+console.log(age);
+function add(x, y) {
+    console.log('Addition of two numbers: ', x + y);
+    return x = y;
+}
+add(21, 78);
+export {};
