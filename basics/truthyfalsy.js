fbb8dd94b2f4d2b79cf27e939e36e610b10d8e8d
vinay-sky182 <vinay.sky182@gmail.com>
2025-08-25 19:12:33 +0530

    These JS concepts moved inside of JavaScript branch

--- basics/truthyfalsy.js
+++
@@ -1,7 +0,0 @@
-// 8 flasy values in js:
-// 0n - BigInt zero
-
-// other than these 8 vlues are considered truthy in JavaScript.
-
-const falsyValues = [false, 0, -0, "", null, undefined, NaN, 0n];
-
