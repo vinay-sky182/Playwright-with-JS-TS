f49580e4892aab213aa7ac3eaafbdcd743294b3d
vinay-sky182 <vinay.sky182@gmail.com>
2025-09-05 00:09:57 +0530

    added "headless: false" property inside of playwright.config.ts file

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless: false,
     trace: 'on-first-retry',
   },
 
