83980c8dc8f6af889269b46c5b0b61d7c87f6d24
vinay-sky182 <vinay.sky182@gmail.com>
2025-09-10 21:39:21 +0530

    added storageState roperty inside playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     headless: false,
     trace: 'on-first-retry',
+    // storageState: 'auth/state.json',
   },
 
   /* Configure projects for major browsers */
