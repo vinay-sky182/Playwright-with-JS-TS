222e011b2e8d27b7e77788c367844198a489f3c1
Vinay Gupta <vigupta@keywordsstudioa.com>
2025-10-01 14:01:12 +0530

    concept of getByRole for checkbox and radio button element

--- src/getByRole.ts
+++ src/getByRole.ts
@@ -9,7 +9,14 @@ import { Browser, chromium, Locator, Page, selectors } from "@playwright/test";
     try {
 
         await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/register");
-        
+
+        await page.getByRole('textbox', { name: 'First Name' }).fill('Naveen');
+        await page.getByRole('textbox', { name: 'Last Name' }).fill('Automation');
+
+        await page.getByRole('checkbox').click();
+        // await page.getByRole('button', { name: 'Continue' }).click();
+        await page.getByRole('radio', { name: 'Yes' }).click();
+
     }
     catch (error) {
         console.log('E:', error);
