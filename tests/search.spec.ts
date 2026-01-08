5c92b471cc5b49d678c53fcaa5d4fbb9d3009320
Vinay Gupta <vigupta@keywordsstudioa.com>
2026-01-08 14:56:34 +0530

    changed the key names within the objects inside the searchData array within search.spec.ts file.

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -7,21 +7,21 @@ import { SearchResultsPage } from '../pages/SearchResultsPage';
  * data provider for product search key and result count
  */
 let searchData = [
-    { searchKey: 'macbook', productCount: 3 },
-    { searchKey: 'samsung', productCount: 2 },
-    { searchKey: 'nikon', productCount: 1 },
-    { searchKey: 'imac', productCount: 1 },
-    { searchKey: 'dummy', productCount: 0 },
+    { searchkey: 'macbook', productcount: 3 },
+    { searchkey: 'samsung', productcount: 2 },
+    { searchkey: 'nikon', productcount: 1 },
+    { searchkey: 'imac', productcount: 1 },
+    { searchkey: 'dummy', productcount: 0 },
 ];
 
 for (let product of searchData) {
 
-    test(`verify product search ${product.searchKey}`, async ({ page }) => {
+    test(`verify product search ${product.searchkey}`, async ({ page }) => {
         let loginPage = new LoginPage(page); // Arange
         await loginPage.goToLoginPage(); // Action 1
         let homePage: HomePage = await loginPage.performLogin('pwtest@nal.com', 'test123'); // Action 2
-        let resultsPage: SearchResultsPage = await homePage.doSearch(product.searchKey); // Action 3
-        expect(await resultsPage.getSearchResultsCount()).toBe(product.productCount); // Assert
+        let resultsPage: SearchResultsPage = await homePage.doSearch(product.searchkey); // Action 3
+        expect(await resultsPage.getSearchResultsCount()).toBe(product.productcount); // Assert
     });
 }
 
