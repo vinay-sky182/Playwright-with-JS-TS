764f039b1dbff7792f445e93e8571fadc045d50f
Vinay Gupta <vigupta@keywordsstudioa.com>
2026-03-09 12:58:27 +0530

    added a comment for Shallow and Deep copy with example

--- basics/object_copy.js
+++ basics/object_copy.js
@@ -34,6 +34,7 @@ let user1 = {
         zip: "10001"
     }
 };
+// JS Object ----> JSON string ------> JS Object
 
 let person1 = JSON.parse(JSON.stringify(user1)); // Deep copy
 
@@ -41,3 +42,28 @@ person1.address.city = "Zurich";
 
 console.log(user1.address.city);   // Output: "New York"
 console.log(person1.address.city); // Output: "Zurich"
+
+
+let user2 = {
+    name: "John Wick",
+    address: {
+        hotel: "Continental Osaka" // Nested Object
+    }
+};
+
+let person1 = { ...user2 }; // Shallow Copy
+
+// Spread operator (...) hamesha ek shallow copy hi banata hai. Iska matlab hai ki yeh sirf "top-level" properties ko naye object mein copy karta hai.
+// Shallow copy ki asliyat tab pata chalti hai jab object ke andar doosra object ho. Agar aap nested object ko change karenge, toh dono objects badal jayenge.
+
+/* 
+1.) Naya Top-Level Container: Computer memory mein ek naya address allocate hota hai sirf bahar wale object (person) ke liye.
+
+2.) Primitive Values: Jo simple values hain (jaise name: "John"), unki nayi copy banti hai naye address par.
+
+3.) Nested Objects (The Catch): Jo andar ke objects ya arrays hain, unka naya address nahi banta. Shallow copy sirf unka purana "memory address" (reference) utha kar naye object mein chipka deti hai.
+*/
+
+person1.address.hotel = "Continental Casablanca";
+
+console.log(user2.address.city); // Output: "Continental Casablanca" (Original bhi change ho gaya!)
