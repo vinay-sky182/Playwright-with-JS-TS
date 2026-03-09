2049ab5d41c7e94752dabbfea0dc83f812d3d0d7
Vinay Gupta <vigupta@keywordsstudioa.com>
2026-03-09 12:56:28 +0530

    Added the comment for initial value in reduce() method

--- basics/reduceconcept.js
+++ basics/reduceconcept.js
@@ -3,6 +3,7 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // sum of all the numbers :15
+// 0 is initial value of sum
 
 let total = numbers.reduce((sum, ele) => sum + ele, 0);
 console.log('sum of all the elements of an array:', total);
@@ -10,6 +11,7 @@ console.log('sum of all the elements of an array:', total);
 console.log("-------------------------------------------------------------------------");
 
 // multiplie all the numbers : 120
+// 1 is initial value of total
 
 let multiply = numbers.reduce((total, num) => total * num, 1);
 console.log('multiplication of all the elements of an array:', multiply);
@@ -18,8 +20,15 @@ console.log("-------------------------------------------------------------------
 
 let dict = ['hello', 'javascript', 'playwright'];
 
+/* Jab aapne initial value nahi di, toh engine ne array ['hello', 'javascript', 'playwright'] ko dekha aur ye steps liye:
+
+Step 1: Engine ne dict[0] (yaani 'hello') ko uthaya aur use result variable mein daal diya.
+
+Step 2: Kyunki pehla element pehle hi accumulator ban gaya, isliye loop seedha doosre element ('javascript') se shuru hua. */
+
 let newDict = dict.reduce((result, ele) => result + ' ' + ele);
 
+console.log('dict:', dict);
 console.log('new dict:', newDict);
 
 console.log("-------------------------------------------------------------------------");
