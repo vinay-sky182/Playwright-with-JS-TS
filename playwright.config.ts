4305ce322914e4778d5536413e4b15f0e28ce351
Vinay Gupta <vinay.sky182@gmail.com>
2026-06-28 19:11:34 +0530

    modified playwright.config.ts file and workflow file

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -32,10 +32,15 @@ export default defineConfig({
     ],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  expect: {
+    timeout: 10000,
+  },
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    navigationTimeout: 15000,
+    actionTimeout: 10000,
     trace: 'on-first-retry',
     headless: true,
     screenshot: 'on',
@@ -55,13 +60,38 @@ export default defineConfig({
     {
       name: "Google Chrome",
       use: {
+        ...devices[ 'Desktop Chrome' ],
         channel: "chrome",
-        viewport: null,
-        launchOptions: {
-          args: [ "--start-maximized" ],
-          ignoreDefaultArgs: [ "--window-size=1920,1080" ],
-        },
+        // viewport: null,
+        // launchOptions: {
+        //   args: [ "--start-maximized" ],
+        //   ignoreDefaultArgs: [ "--window-size=1920,1080" ],
+        // },
       },
+    },
+
+    {
+      name: 'Microsoft Edge',
+      use: {
+        ...devices[ 'Desktop Edge' ],
+        channel: 'msedge',
+      }
+    },
+
+    {
+      name: 'Firefox',
+      use: {
+        ...devices[ 'Desktop Firefox' ],
+        browserName: 'firefox',
+      }
+    },
+
+    {
+      name: 'Safari',
+      use: {
+        ...devices[ 'Desktop Safari' ],
+        browserName: 'webkit',
+      }
     },
 
     // {
